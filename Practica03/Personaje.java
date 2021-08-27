46f92d432a8bcf2558a63a21bcd791fe61f49862
unknown <jesus_ricardo166@hotmail.com>
2021-08-27 08:09:20 -0500

    Error 2

--- Practica03/Personaje.java
+++ Practica03/Personaje.java
@@ -2,13 +2,13 @@ public class Personaje{
 	private String nombre;
 	private int edad;
 
-	public void setNombre(nombre){
+	public void setNombre(String nombre){
 		this.nombre=nombre;
 	}
 	public String getNombre(){
 		return nombre;
 	}
-	public int saludar(){
-		System.out.println("Hola alumno de POO" + nombre);
+	public void saludar(){
+		System.out.println("Hola alumno de POO " + nombre);
 	}
 }
