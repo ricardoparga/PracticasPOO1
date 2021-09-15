df5cd46275132a35f2d9a733235932e7c9e5a7a6
unknown <jesus_ricardo166@hotmail.com>
2021-09-15 09:55:05 -0500

    Pasos 5 y 6

--- Practica06/src/mx/com/softgame/poo1game/pruebas/TestPersonaje.java
+++ Practica06/src/mx/com/softgame/poo1game/pruebas/TestPersonaje.java
@@ -2,20 +2,18 @@ package mx.com.softgame.poo1game.pruebas;
 import mx.com.softgame.poo1game.personajes.Personaje;
 public class TestPersonaje{
 	public static void main(String[] args) {
-		Personaje alumno = new Personaje("Kal-EL");
-		alumno.saludar();
-		System.out.println("Nombre: " + alumno.getNombre() + "Edad: " + alumno.getEdad());
-		System.out.println("Modificando el nombre: " + alumno.getNombre());
-		alumno.setNombre("Michael Jordan");
-		alumno.saludar();
-		if (!alumno.setEdad(30)) {
-			System.out.println("Edad sin cambios: ");
-		}
-		System.out.println("Nombre: " + alumno.getNombre() + "Edad: " + alumno.getEdad());
-		System.out.println("Modificando edad en 130");
-		if (!alumno.setEdad(130)) {
-			System.out.println("Edad sin cambios: ");
-		}
-		System.out.println("Nombre: " + alumno.getNombre() + "Edad: " + alumno.getEdad());
+		Personaje per01 = new Personaje ("Aline", 20);
+		Personaje per02 = new Personaje ("Valeria", 10);
+		Personaje per03 = new Personaje ("Federico", 1);
+		Personaje per04 = new Personaje ("Mayela", -10);
+		Personaje per05 = new Personaje ("Edwin", 500);
+
+		System.out.println(per01.getDetalle());
+		System.out.println(per02.getDetalle());
+		System.out.println(per03.getDetalle());
+		System.out.println(per04.getDetalle());
+		System.out.println(per05.getDetalle());
+
+		
     }
 }
