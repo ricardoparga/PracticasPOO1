a8dc0bf6477717b7da295d84a1a6a193abb2148f
unknown <jesus_ricardo166@hotmail.com>
2021-09-17 08:22:04 -0500

    Paso 6
    Falta condicion de atributo poder, aun no se como hacerlo.

--- Practica06_07/src/mx/tv/kishimoto/naruto/anime/Personaje.java
+++ Practica06_07/src/mx/tv/kishimoto/naruto/anime/Personaje.java
@@ -1,4 +1,20 @@
 package mx.tv.kishimoto.naruto.anime;
 public class Personaje{
+	private String nombre;
+	private int poder;
+
+	public Personaje(String nombre, int poder){
+		this.nombre = nombre;
+		int edad = 0;
+	}
+
+	public void setPoder(int poder){
+
+	}
+
+	public String getDatos(){
+		return nombre + "\t" + poder;
+	}
+
 	
 }
