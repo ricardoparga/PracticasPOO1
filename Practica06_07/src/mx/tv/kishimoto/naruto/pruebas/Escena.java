45a63acd3d289f5cfd0db791d7937d06e064432b
unknown <jesus_ricardo166@hotmail.com>
2021-09-17 08:29:32 -0500

    Paso 8

--- Practica06_07/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
+++ Practica06_07/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
@@ -1,5 +1,16 @@
 package mx.tv.kishimoto.naruto.pruebas;
 import mx.tv.kishimoto.naruto.anime.Personaje;
 public class Escena{
-	
+	public static void main(String[] args) {
+		Personaje naruto = new Personaje ("Naruto", 1000);
+		Personaje sasuke = new Personaje ("Sasuke", -10);
+		Personaje sakura = new Personaje ("Sakura", 300);
+
+		System.out.println(naruto.getDetalle());
+		System.out.println(sasuke.getDetalle());
+		System.out.println(sakura.getDetalle());
+		System.out.println("_ _ _");
+
+		
+	}
 }
