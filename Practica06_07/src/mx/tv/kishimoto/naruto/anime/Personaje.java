218b7004be6ebf8e728e6cd159d044b55f02245c
unknown <jesus_ricardo166@hotmail.com>
2021-09-17 08:03:52 -0500

    Paso 4

---
+++ Practica06_07/src/mx/tv/kishimoto/naruto/anime/Personaje.java
@@ -0,0 +1,4 @@
+package mx.tv.kishimoto.naruto.anime;
+public class Personaje{
+	
+}
---
+++ Practica06_07/src/mx/tv/kishimoto/naruto/pruebas/Escena.java
@@ -0,0 +1,5 @@
+package mx.tv.kishimoto.naruto.pruebas;
+import mx.tv.kishimoto.naruto.anime.Personaje;
+public class Escena{
+	
+}
