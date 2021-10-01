606c6f256ba7e00b52c687544912bc487d2718ea
unknown <jesus_ricardo166@hotmail.com>
2021-10-01 09:02:31 -0500

    Inicio paso 6

---
+++ Practica08/src/mx/com/softgame/poo1game/personajes/malos/Zombie.java
@@ -0,0 +1,18 @@
+package mx.com.softgame.poo1game.personajes.malos;
+import mx.com.softgame.poo1game.personajes.Personaje;
+
+public class Zombie extends Personaje{
+	private boolean ataque;
+
+	public Zombie(String nombre, int vida, boolean ataque){
+		super(nombre, vida);
+		this.ataque = ataque;
+	}
+	public Zombie(String nombre, boolean ataque){
+		this(nombre, 3, ataque);
+	}
+	
+	public boolean getAtaque(){
+		return ataque;
+	}
+}
