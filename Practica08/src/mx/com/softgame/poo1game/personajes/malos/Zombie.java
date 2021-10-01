4bb57449d05102287ae6b824f5e5adc1ed0700a4
unknown <jesus_ricardo166@hotmail.com>
2021-10-01 09:04:11 -0500

    Final paso 6

--- Practica08/src/mx/com/softgame/poo1game/personajes/malos/Zombie.java
+++ Practica08/src/mx/com/softgame/poo1game/personajes/malos/Zombie.java
@@ -11,8 +11,15 @@ public class Zombie extends Personaje{
 	public Zombie(String nombre, boolean ataque){
 		this(nombre, 3, ataque);
 	}
+	public Zombie(String nombre){
+		this(nombre, 3, false);
+	}
 	
+
 	public boolean getAtaque(){
 		return ataque;
+	}
+	public String getDetalle(){
+		return super.getDetalle() + "\t" + ataque;
 	}
 }
