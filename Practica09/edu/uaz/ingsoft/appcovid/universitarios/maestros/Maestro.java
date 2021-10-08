7de69a4ba5234575b914a1d08203b4011854b9ec
unknown <jesus_ricardo166@hotmail.com>
2021-10-08 10:29:49 -0500

    Paso 5
    Import

--- Practica09/edu/uaz/ingsoft/appcovid/universitarios/maestros/Maestro.java
+++ Practica09/edu/uaz/ingsoft/appcovid/universitarios/maestros/Maestro.java
@@ -1,4 +1,5 @@
 package edu.uaz.ingsoft.appcovid.universitarios.maestros;
+import edu.uaz.ingsoft.appcovid.universitarios.Universitario;
 public class Maestro extends Universitario{
 	float sueldo;
 
