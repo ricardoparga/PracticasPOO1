c1ba52cb697090bcb18f1d1de2305753603be9c9
unknown <jesus_ricardo166@hotmail.com>
2021-10-08 10:36:48 -0500

    Paso 6

---
+++ Practica09/edu/uaz/ingsoft/appcovid/universitarios/alumnos/Alumno.java
@@ -0,0 +1,34 @@
+package edu.uaz.ingsoft.appcovid.universitarios.alumnos;
+import edu.uaz.ingsoft.appcovid.universitarios.Universitario;
+public class Alumno{
+	int calificacion;
+
+	public Alumno(String nombre, int calificacion){
+		super(nombre);
+		this.calificacion = calificacion;
+		if (calificacion>0 && calificacion<10) {
+			System.out.println(calificacion);
+		} else {
+			calificacion = 5;
+			System.out.println(calificacion);
+		}
+	}
+	public Alumno(String nombre){
+		this(nombre, 5);
+	}
+
+	public boolean equals(Object o){
+        boolean iguales = false;
+        if (o != null && o instanceof Alumno) {
+        	Alumno a = (Alumno) o;
+        	if (nombre == a.nombre && calificacion == a.calificacion) {
+        		iguales = true;
+        	}
+        }
+        return iguales;
+	}
+
+	public String toString(){
+		return super.toString() + calificacion;
+	}
+}
