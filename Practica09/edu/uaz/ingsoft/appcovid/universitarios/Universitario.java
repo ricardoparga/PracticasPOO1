e6a09e5198040f6a1c75d85a895a2403aedb4184
unknown <jesus_ricardo166@hotmail.com>
2021-10-08 10:20:06 -0500

    Paso 4

---
+++ Practica09/edu/uaz/ingsoft/appcovid/universitarios/Universitario.java
@@ -0,0 +1,30 @@
+package edu.uaz.ingsoft.appcovid.universitarios;
+public class Universitario{
+	String nombre;
+
+	public Universitario(String nombre){
+		this.nombre = nombre;
+	}
+
+	public boolean equals(Object o){
+        boolean iguales = false;
+        if (o != null && o instanceof Universitario) {
+        	Universitario u = (Universitario) o;
+        	if (nombre == u.nombre) {
+        		iguales = true;
+        	}
+        }
+        return iguales;
+	}
+
+	@Override
+	public int hashCode(){
+		int hash = 1;
+		hash = hash * 31 + ((nombre == null) ? 0 : nombre.hashCode());
+		return hash;
+	} 
+
+	public String toString(){
+		return nombre;
+	}
+}
