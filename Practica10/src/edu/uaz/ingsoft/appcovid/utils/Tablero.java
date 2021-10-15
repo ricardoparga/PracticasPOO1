b36b655a515deb11182575f3272fc4fc04be2ae6
unknown <jesus_ricardo166@hotmail.com>
2021-10-15 08:36:53 -0500

    Practica10, 8:36

--- Practica10/src/edu/uaz/ingsoft/appcovid/utils/Tablero.java
+++ Practica10/src/edu/uaz/ingsoft/appcovid/utils/Tablero.java
@@ -2,4 +2,20 @@ package edu.uaz.ingsoft.appcovid.utils;
 import edu.uaz.ingsoft.appcovid.universitarios.Universitario;
 public class Tablero{
 	static final int MAX_LINE = 5;
+	private static int poss = -1;
+	private Universitario [] personas = new Universitario {MAX_LINE};
+	
+	private Tablero(){
+	}
+
+	private static void mostrar(){
+		System.out.println("*El tablero contiene: *");
+		for (Universitario u : personas) {
+			System.out.println(u);
+			if (u == null) {
+				System.out.println("-");
+			}
+		}
+		System.out.println("******");
+	}
 }
