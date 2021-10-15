a8ea86f89cdcc4fa4c4014ac499597877fb97b78
unknown <jesus_ricardo166@hotmail.com>
2021-10-15 10:06:21 -0500

    Practica10, 10:06

--- Practica10/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaUniv.java
+++ Practica10/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaUniv.java
@@ -7,6 +7,17 @@ import edu.uaz.ingsoft.appcovid.utils.Tablero;
 public class PruebaUniv{
 	public static void main(String[] args) {
 		Tablero.mostrar();
-		
+		Tablero.insertar{new Universitario("Jose Miguel"),
+			new Maestro(("Giovanna Esmeralda", 10f), 4),
+			new Alumno("Hanna Jimena", 80),
+			new Universitario("Saul Alfonso"),
+			new Maestro(("Edwin Brayan", 5000f), 6)
+			new Alumno("Esther Sarahi", 5)
+		};
+		Tablero.mostrar();
+		Tablero.borrar(personas[4]);
+		Tablero.borrar();
+		Tablero.borrar(personas[10]);
+		Tablero.mostrar();
 	}
 }
