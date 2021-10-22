78cbe0a7eb3a246edbade8e3a1a8ca231d9702cc
unknown <jesus_ricardo166@hotmail.com>
2021-10-22 12:41:42 -0500

    Create PruebaEnum.java

---
+++ Practica11/src/edu/uaz/ingsoft/appcovid/pruebas/PruebaEnum.java
@@ -0,0 +1,19 @@
+package edu.uaz.ingsoft.appcovid.pruebas;
+import edu.uaz.ingsoft.appcovid.universitarios.Universitario;
+import edu.uaz.ingsoft.appcovid.universitarios.maestros.Maestro;
+import edu.uaz.ingsoft.appcovid.universitarios.alumnos.Alumno;
+public class PruebaEnum{
+	public static void main(String[] args) {
+		Universitario [] a = new Alumno("Ricardo");
+
+		private Calificacion getCalifAleatoria(){
+			int rnm = (int)(Math.random()*10);
+		}
+
+		int x = 0;
+		while (x < 6){
+			System.out.println(a);
+
+		}
+	}
+}
