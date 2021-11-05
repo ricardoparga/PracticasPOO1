670b901f12bac849c9866aa6a24e5d884b3f322e
unknown <jesus_ricardo166@hotmail.com>
2021-11-05 10:26:33 -0600

    Practica13, Paso3

--- Practica13/src/edu/uaz/ingsoft/appcovid/utils/Tablero.java
+++ Practica13/src/edu/uaz/ingsoft/appcovid/utils/Tablero.java
@@ -1,13 +1,13 @@
 package edu.uaz.ingsoft.appcovid.utils;
 import edu.uaz.ingsoft.appcovid.universitarios.Universitario;
-
-
 public class Tablero{
 	private static final int MAX = 5;
 	private static int pos = -1;
 	private static Universitario[] personas = new Universitario[MAX];
 
-	private Tablero(){}
+	private Tablero(){
+
+	}
 
 	public static void mostrar(){
 		System.out.println("*El tablero contiene*");
@@ -21,39 +21,46 @@ public class Tablero{
 		}
 		System.out.println(" * * * * * ");
 	}
-	public static void insertar(Universitario u){
+
+	public static void insertar(Universitario u) throws TableroException {
 		if ((pos+1 > -1) && (pos+1 < MAX) && (personas[pos+1] == null)){
 			personas[pos+1] = u;
 			pos++;
 		} else {
-			System.out.println("Imposible insertar en: " + (pos+1) );
+			throw new TableroException("Imposible insertar en: " + (pos+1) );
 		}
 	}
 
-	public static void insertar(Universitario u, int idx){
+	public static void insertar(Universitario u, int idx) throws TableroException {
 		if ((idx > -1) && (idx < MAX) && (personas[idx] == null)){
 			personas[idx] = u;
 		} else {
-			System.out.println("Imposible insertar en: " + idx );
+			throw new TableroException("Imposible insertar en: " + idx );
 		}
 	}
 
-	public static void borrar(){
+	public static void borrar() throws TableroException {
 		System.out.println(pos);
 		if ((pos > -1 ) && (pos < MAX)){
 		 	personas[pos] = null;
 			pos--;
 		 } else {
-		 	System.out.println("Imposible borrar en: " + pos);
+		 	throw new TableroException("Imposible borrar en: " + pos);
 		 }
 	}
 
-	public static void borrar(int idx){
+	public static void borrar(int idx) throws TableroException {
 		if ((idx > -1 ) && (idx < MAX)){
 		 	personas[idx] = null;
 		 } else {
-		 	System.out.println("Imposible borrar en: " + idx);
+		 	throw new TableroException("Imposible borrar en: " + idx);
 		 }
+	}
+
+	public void getPersonas(){
+		for (Universitario universitario : universitarios) {
+			System.out.println(universitario);
+		}
 	}
 
 }
