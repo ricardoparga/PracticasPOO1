60e0eb22dedb38f4be309f358506a50a47f17b7d
unknown <jesus_ricardo166@hotmail.com>
2021-11-12 11:45:45 -0600

    Practica14, 11:45

--- Practica14/src/edu/uaz/ingsoft/appcovid/cursos/Materia.java
+++ Practica14/src/edu/uaz/ingsoft/appcovid/cursos/Materia.java
@@ -1,4 +1,5 @@
 package edu.uaz.ingsoft.appcovid.cursos;
+import java.lang.Comparable;
 public class Materia implements Comparable <Materia> {
 	private final String clase;
     private int semestre;
@@ -13,14 +14,37 @@ public class Materia implements Comparable <Materia> {
     public final String getClase(){
     	return clase;
     }
-
     public int getSemestre() {
         return semestre;
     }
-
     public char getGrupo() {
         return grupo;
     }
 
-    
+    public void setSemestre(int semestre) {
+        if((semestre>=1) && (semestre<=16)){
+            this.semestre=semestre;
+        }
+    }
+    public void setGrupo(char grupo) {
+        if(Character.isAlphabetic(grupo)){
+            this.grupo = grupo;
+        }
+    }
+
+    public int compareTo(Materia m) {
+        if (clase.compareTo(m.clase)!= 0) {
+            return clase.compareTo(m.clase);
+        }
+
+        if ((semestre - m.semestre)!=0) {
+            return semestre - m.semestre;
+        }
+
+        return ((int)grupo) - ((int)m.grupo);
+    }
+
+    public String toString() {
+        return clase + "\t" + semestre + "\t " + grupo;
+    }
 }
