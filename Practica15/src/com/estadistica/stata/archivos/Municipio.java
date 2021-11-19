2fccef4e8c3dc33f4d5c5c859b03987870621d48
unknown <jesus_ricardo166@hotmail.com>
2021-11-19 11:37:07 -0600

    Practica15, 11;37

---
+++ Practica15/src/com/estadistica/stata/archivos/Municipio.java
@@ -0,0 +1,25 @@
+package com.estadistica.stata;
+public class Municipio{
+	private short id;
+	private String nombre ="";
+	private int poblacion = 0;
+
+	public Municipio(short id, String nombre, int poblacion){
+		this.id = id;
+		this.nombre = nombre;
+		this.poblacion = poblacion;
+	}
+
+	public short getId(){
+		return id;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+	public int getPoblacion(){
+		return poblacion;
+	}
+	public String toString(){
+		return "{"+ id +" \t "+ poblacion +"}"
+	}
+}
