443fa60e7de4f12b0006b7e89ce6a3de5647bdef
unknown <jesus_ricardo166@hotmail.com>
2021-11-26 15:19:46 -0600

    Practica16, 3:19

--- Practica16/src/mx/edu/uaz/ingsoft/hilos/ventanas/Ventana.java
+++ Practica16/src/mx/edu/uaz/ingsoft/hilos/ventanas/Ventana.java
@@ -1,18 +1,49 @@
 package mx.edu.uaz.ingsoft.hilos.ventanas;
 import mx.edu.uaz.ingsoft.hilos.elementos.Elemento;
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
 
 public class Ventana extends JFrame{
 	public Acciones acc;
 	private JMenuBar mb;
 	private JMenu menu;
-	private JMenuItem iniciar;
-	private JMenuItem salir;
+	private JMenuItem miStart;
+	private JMenuItem miExit;
 
 	public Ventana(){
 		acc = new Acciones(this);
-		
+		mb = new JMenuBar();
+		setJMenuBar(mb);
+		menu = new JMenu("Archivo");
+		mb.add(menu);
+		setJMenuBar(mb);
+		miStart = new JMenuItem("Iniciando hilo...");
+		miExit = new JMenuItem("Saliendo del hilo...");
+		menu.add(miStart);
+
+		miStart.addActionListener(new addActionListener(){
+			public void actionPerformed(ActionEvent e){
+				acc.iniciar();
+			}
+		});
+
+		menu.addSeparator();
+		menu.add(miExit);
+
+		miExit.addActionListener(new addActionListener(){
+			public void actionPerformed(ActionEvent e){
+				dispose();
+			}
+		});
+
+		JLabel elem1 = new JLabel("Elemento1");
+		JLabel elem2 = new JLabel("Elemento2");
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setJMenuBar(mb);
 	}
 
+	
 }
